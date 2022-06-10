798a695f0c2ddbc643239716ff8dd6c029e0b430
HarunTeper <hteper@web.de>
2022-06-10 15:39:39 +0200

    added StringArray type and changed artery node to use stringarray instead of string

--- src/car_simulator/src/omnet/artery_info.cpp
+++ src/car_simulator/src/omnet/artery_info.cpp
@@ -7,6 +7,7 @@
 #include "std_msgs/msg/string.hpp"
 #include "gazebo_msgs/srv/get_model_list.hpp"
 #include "gazebo_msgs/msg/model_states.hpp"
+#include "ros_its_msgs/msg/string_array.hpp"
 
 using namespace std::placeholders;
 
@@ -17,27 +18,30 @@ class arteryInfo : public rclcpp::Node
         // Create the publisher, timer and service client
         arteryInfo(): Node("artery_info_node"), robot_names()
         {
-            subscription = this->create_subscription<gazebo_msgs::msg::ModelStates>("/model_states", 10, std::bind(&arteryInfo::model_state_callback, this, _1));
-            publisher = this->create_publisher<std_msgs::msg::String>("/artery_model_list", 10);
+            subscription = this->create_subscription<gazebo_msgs::msg::ModelStates>("/model_states", 1, std::bind(&arteryInfo::model_state_callback, this, _1));
+            publisher = this->create_publisher<ros_its_msgs::msg::StringArray>("/artery_model_list", 1);
         }
 
     private:
         // Timer callback which starts the service request to Gazebo
 
         void model_state_callback(const gazebo_msgs::msg::ModelStates::SharedPtr msg){
+            std::vector<std::string> robot_names;
             for(std::string model_name : msg->name)
             {
                 if(model_name.find("robot") != std::string::npos)
                 {
-                    auto message = std_msgs::msg::String();
-                    message.data = model_name;
-                    publisher->publish(message);
+                    robot_names.push_back(model_name);
                 }
             }
+
+            auto message = ros_its_msgs::msg::StringArray();
+            message.strings = robot_names;
+            publisher->publish(message);
         }
 
         rclcpp::Subscription<gazebo_msgs::msg::ModelStates>::SharedPtr subscription;
-        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher;
+        rclcpp::Publisher<ros_its_msgs::msg::StringArray>::SharedPtr publisher;
         rclcpp::TimerBase::SharedPtr service_timer;
         std::vector<std::string> robot_names;
 };
