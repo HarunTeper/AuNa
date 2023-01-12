799a9db4249d9b2efab3a0b6d0652a2cc52e9a96
harun <hteper@web.de>
2023-01-12 16:35:52 +0100

    renamed service for cacc filter

--- src/auna_omnet/src/omnet/omnet_cam_filter/omnet_cam_filter.cpp
+++ src/auna_omnet/src/omnet/omnet_cam_filter/omnet_cam_filter.cpp
@@ -4,7 +4,7 @@ OmnetCamFilter::OmnetCamFilter(int identifier) : Node("omnet_cam_filter_node")
 {
     cam_subscriber_ = this->create_subscription<auna_its_msgs::msg::CAM>("cam", 2, [this](const auna_its_msgs::msg::CAM::SharedPtr msg){cam_callback(msg);});
     cam_publisher_ = this->create_publisher<auna_its_msgs::msg::CAM>("cam_filtered", 2);
-    service_ = this->create_service<auna_msgs::srv::Identifier>("cam_filter_names", [this](const std::shared_ptr<auna_msgs::srv::Identifier::Request> request, std::shared_ptr<auna_msgs::srv::Identifier::Response> response){filter_service_callback(request,response);});
+    service_ = this->create_service<auna_msgs::srv::Identifier>("cam_filter_index", [this](const std::shared_ptr<auna_msgs::srv::Identifier::Request> request, std::shared_ptr<auna_msgs::srv::Identifier::Response> response){filter_service_callback(request,response);});
     identifier_ = identifier;
 }
 
