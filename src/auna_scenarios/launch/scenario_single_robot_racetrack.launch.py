14dd1648a728a5ea5cda725bc5863a7899856537
HarunTeper <hteper@web.de>
2023-04-30 18:30:30 +0200

    removed unused launch file imports

--- src/auna_scenarios/launch/scenario_single_robot_racetrack.launch.py
+++ src/auna_scenarios/launch/scenario_single_robot_racetrack.launch.py
@@ -1,7 +1,6 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument,IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
