fd453b9006a7751f954c7e339a0e492ab6814f39
HarunTeper <hteper@web.de>
2023-05-16 12:42:37 +0200

    added mqtt and json to auto_setup.sh

--- auto_setup.sh
+++ auto_setup.sh
@@ -31,4 +31,27 @@ sudo usermod -a -G dialout $USER
 
 sudo apt-get -y install ros-galactic-ackermann-msgs ros-galactic-diagnostic-* ros-galactic-geographic-* ros-galactic-joy* ros-galactic-laser* ros-galactic-realsense2-* ros-galactic-robot-localization* ros-galactic-teleop-twist-joy* ros-galactic-serial-driver ros-galactic-udp-* ros-galactic-asio-cmake-module ros-galactic-io-context ros-galactic-urg-node* 
 
+# MQTT
+git clone https://github.com/eclipse/paho.mqtt.c.git ~/paho.mqtt.c
+cd ~/paho.mqtt.c
+git checkout v1.3.8
+cmake -Bbuild -H. -DPAHO_ENABLE_TESTING=OFF -DPAHO_BUILD_STATIC=ON \
+    -DPAHO_WITH_SSL=ON -DPAHO_HIGH_PERFORMANCE=ON
+sudo cmake --build build/ --target install
+sudo ldconfig
+
+git clone https://github.com/eclipse/paho.mqtt.cpp ~/paho.mqtt.cpp
+cd ~/paho.mqtt.cpp
+cmake -Bbuild -H. -DPAHO_BUILD_STATIC=ON \
+    -DPAHO_BUILD_DOCUMENTATION=TRUE -DPAHO_BUILD_SAMPLES=TRUE
+sudo cmake --build build/ --target install
+sudo ldconfig
+
+# JSON
+sudo apt-get -y install nlohmann-json3-dev
+
+# Build
+
+cd ~/AuNa
+source /opt/ros/galactic/setup.bash
 colcon build --symlink-install
