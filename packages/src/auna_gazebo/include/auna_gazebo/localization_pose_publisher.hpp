757fb5bdac36efaf8811b76f557d1d80af970857
felloeh <loehfelix@gmail.com>
2024-12-18 13:43:06 +0000

    Refactor localization pose handling: rename and replace localization_pose with localization_pose_publisher, update CMake configuration, and remove obsolete files

---
+++ packages/src/auna_gazebo/include/auna_gazebo/localization_pose_publisher.hpp
@@ -0,0 +1,27 @@
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "std_msgs/msg/header.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
+class LocalizationPosePublisher : public rclcpp::Node
+{
+public:
+  LocalizationPosePublisher(std::string prefix);
+
+private:
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) const;
+
+  tf2_ros::Buffer buffer;
+  tf2_ros::TransformListener listener;
+  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher;
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  void timer_callback();
+
+  std::string prefix;
+};
