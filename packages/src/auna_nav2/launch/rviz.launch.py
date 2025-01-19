3648561d251b400b9ead5fd7edbe5a3be77bac0d
felloeh <loehfelix@gmail.com>
2025-01-19 15:56:33 +0000

    Add logging for RViz2 startup and clean up namespace handling in launch file

--- packages/src/auna_nav2/launch/rviz.launch.py
+++ packages/src/auna_nav2/launch/rviz.launch.py
@@ -22,6 +22,7 @@ def include_launch_description(context: LaunchContext):
     rviz_config_file = LaunchConfiguration('rviz_config')
 
     # Launch rviz
+    print('Starting RViz2 with namespace: ' + namespace.perform(context))
     if namespace.perform(context) == '':
         namespaced_rviz_config_file = ReplaceString(
             source_file=rviz_config_file,
@@ -29,14 +30,14 @@ def include_launch_description(context: LaunchContext):
         base_frame = ''
     else:
         namespaced_rviz_config_file = ReplaceString(
-                source_file=rviz_config_file,
-                replacements={'<robot_namespace>': ('/', namespace)})
+            source_file=rviz_config_file,
+            replacements={'<robot_namespace>': ('/', namespace)})
         base_frame = namespace.perform(context)+'/'+'base_link  '
 
     start_rviz_cmd = Node(
         package='rviz2',
         executable='rviz2',
-        namespace=namespace,
+        namespace='',  # remove namespace to avoid duplicated namespace
         arguments=['-d', namespaced_rviz_config_file],
         output='screen',
         remappings=[('/tf', 'tf'),
@@ -62,6 +63,7 @@ def include_launch_description(context: LaunchContext):
 
     return launch_description_content
 
+
 def generate_launch_description():
     """Launch RViz2 with the default view for the navigation stack. """
     # Get the launch directory
@@ -76,7 +78,8 @@ def generate_launch_description():
 
     declare_rviz_config_file_cmd = DeclareLaunchArgument(
         'rviz_config',
-        default_value=os.path.join(pkg_dir, 'rviz', 'config_navigation_namespace.rviz'),
+        default_value=os.path.join(
+            pkg_dir, 'rviz', 'config_navigation_namespace.rviz'),
         description='Full path to the RVIZ config file to use')
 
     # Create the launch description and populate
@@ -85,6 +88,7 @@ def generate_launch_description():
     # Declare the launch options
     launch_description.add_action(declare_namespace_cmd)
     launch_description.add_action(declare_rviz_config_file_cmd)
-    launch_description.add_action(OpaqueFunction(function=include_launch_description))
+    launch_description.add_action(OpaqueFunction(
+        function=include_launch_description))
 
     return launch_description
