bbf09a8537ceaf3d4d8f6c4342059becd26837c9
felloeh <felix.loehring001@stud.fh-dortmund.de>
2025-07-16 09:42:58 +0000

    merging auna_physical into auna_f110

--- packages/src/auna_physical/src/cmd_vel_to_ackermann/cmd_vel_to_ackermann_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "auna_physical/cmd_vel_to_ackermann.hpp"
-
-int main(int argc, char * argv[])
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<CmdVelToAckermann>());
-    rclcpp::shutdown();
-    return 0;
-}
