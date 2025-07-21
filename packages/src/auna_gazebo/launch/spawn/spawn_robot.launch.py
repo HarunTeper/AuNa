523d25cfac8773b16d590d5aa24669b60493df9b
HarunTeper <harun.teper@tu-dortmund.de>
2025-07-21 18:28:44 +0200

    feat: update spawn command in docker-compose and add new robot spawn launch file

--- packages/src/auna_gazebo/launch/spawn/spawn_robot.launch.py
+++ packages/src/auna_gazebo/launch/spawn/spawn_robot.launch.py
@@ -5,22 +5,20 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node, SetRemap, PushRosNamespace
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction, OpaqueFunction, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
-from launch.substitutions import PythonExpression
+from launch.substitutions import PythonExpression, LocalSubstitution
+from launch.actions import ExecuteProcess, RegisterEventHandler, LogInfo
+from launch.launch_context import LaunchContext
+from launch.event_handlers import OnShutdown
 
 
-def generate_launch_description():
+def include_launch_description(context: LaunchContext):
     """Return launch description"""
     # Package Directories
     pkg_dir = get_package_share_directory('auna_gazebo')
-
-    # Paths to folders and files
-    spawn_launch_file_dir = os.path.join(
-        pkg_dir, 'launch', 'spawn')  # Renamed for clarity
-    # auna_ekf_pkg_share = get_package_share_directory('auna_ekf')
-    # ekf_launch_file_dir = os.path.join(auna_ekf_pkg_share, 'launch') # Path for EKF launch files from auna_ekf
+    spawn_launch_file_dir = os.path.join(pkg_dir, 'launch', 'spawn')
 
     # Launch Configurations
     name = LaunchConfiguration('name')
@@ -31,45 +29,6 @@ def generate_launch_description():
     z_pose = LaunchConfiguration('z_pose')
     ground_truth = LaunchConfiguration('ground_truth')
     debug_ekf = LaunchConfiguration('debug_ekf')
-
-    # Launch Arguments
-    launch_args = [
-        DeclareLaunchArgument(
-            'name',
-            default_value='robot',
-            description='Robot name in Gazebo'
-        ),
-        DeclareLaunchArgument(
-            'namespace',
-            default_value='robot',
-            description='Robot namespace for ROS nodes and topics'
-        ),
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='true',
-            description='Use simulation (Gazebo) clock if true'
-        ),
-        DeclareLaunchArgument(
-            'x_pose',
-            default_value='0.0',
-            description='Robot spawn x position'
-        ),
-        DeclareLaunchArgument(
-            'y_pose',
-            default_value='0.0',
-            description='Robot spawn y position'
-        ),
-        DeclareLaunchArgument(
-            'z_pose',
-            default_value='0.0',
-            description='Robot spawn z position'
-        ),
-        DeclareLaunchArgument(
-            'ground_truth',
-            default_value='False',
-            description='Use ground truth pose'
-        )
-    ]
 
     # Individual components
     tf_remap = SetRemap(src='/tf', dst='tf')
@@ -145,13 +104,64 @@ def generate_launch_description():
         # tf_static_remap,
         robot_state_publisher,
         spawn_robot,
+        # delete_entity_service
         # localization_pose_publisher,
         # ground_truth_cam,
         # ground_truth_transform,
         # ground_truth_pose_publisher,
     ])
 
+    launch_actions = []
+    launch_actions.append(robot_launch_group)
+    return launch_actions
+
+
+def generate_launch_description():
+
+    # Launch Arguments
+    name_arg = DeclareLaunchArgument(
+        'name',
+        default_value='robot',
+        description='Robot name in Gazebo'
+    )
+    namespace_arg = DeclareLaunchArgument(
+        'namespace',
+        default_value='robot',
+        description='Robot namespace for ROS nodes and topics'
+    )
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'
+    )
+    x_pose_arg = DeclareLaunchArgument(
+        'x_pose',
+        default_value='0.0',
+        description='Robot spawn x position'
+    )
+    y_pose_arg = DeclareLaunchArgument(
+        'y_pose',
+        default_value='0.0',
+        description='Robot spawn y position'
+    )
+    z_pose_arg = DeclareLaunchArgument(
+        'z_pose',
+        default_value='0.0',
+        description='Robot spawn z position'
+    )
+    declare_ground_truth_arg = DeclareLaunchArgument(
+        'ground_truth',
+        default_value='False',
+        description='Use ground truth pose'
+    )
+
     return LaunchDescription([
-        *launch_args,
-        robot_launch_group
+        name_arg,
+        namespace_arg,
+        use_sim_time_arg,
+        x_pose_arg,
+        y_pose_arg,
+        z_pose_arg,
+        declare_ground_truth_arg,
+        OpaqueFunction(function=include_launch_description)
     ])
